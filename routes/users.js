0163013043a104e71c81d731ae86a0baaaf0e358
gustavnogood <gustav.wv.johansson@gmail.com>
2023-05-12 16:23:19 +0200

    added HBS and other things
    added hbs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/Users-model');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', (req, res) => {
+  res.render('signup.hbs', { title: 'Express' });
 });
+
+/* router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = new User({ username, email, password });
+  try {
+    await user.save();
+    req.session.userId = user._id;
+    res.redirect('/');
+  } catch (err) {
+    res.render('error.hbs', { error: err.message });
+  }
+}); */
 
 module.exports = router;
